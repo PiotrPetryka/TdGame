82995c246af7557b59d20cd5d86a01c47954a245
Piotr Petryka <ppwxp19@gmail.com>
2022-12-28 21:23:11 +0100

    I learned how to add image to java window

--- Game/src/main/Game.java
+++ Game/src/main/Game.java
@@ -1,18 +1,35 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 public class Game extends JFrame {
     private GameScreen gameScreen;
+    private BufferedImage img;
     public Game(){
+        importImg();
     setSize(640,640);
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
-    gameScreen = new GameScreen();
+    gameScreen = new GameScreen(img);
     add(gameScreen);
     }
+
+    private void importImg() {
+
+        InputStream is = getClass().getResourceAsStream("../spriteatlas.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
 
--- Game/src/main/GameScreen.java
+++ Game/src/main/GameScreen.java
@@ -2,23 +2,28 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 public class GameScreen extends JPanel {
     private Random random;
+    private BufferedImage img;
 
-    public GameScreen(){
+    public GameScreen(BufferedImage img){
         random = new Random();
+        this.img = img;
 
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        for (int x=0; x < 20; x++){
-            for(int y=0; y<20; y++){
-            g.setColor(getRandomColor());
-            g.fillRect(x*32, y*32, 32,32);
-        }}
+        g.drawImage(img, 0, 0, null);
+
+//        for (int x=0; x < 20; x++){
+//            for(int y=0; y<20; y++){
+//            g.setColor(getRandomColor());
+//            g.fillRect(x*32, y*32, 32,32);
+//        }}
     }
     private Color getRandomColor(){
         int r = random.nextInt(256);
