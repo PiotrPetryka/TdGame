e75778dfd8c68f622e2df755849e9a7fb9d50381
Piotr Petryka <ppwxp19@gmail.com>
2022-12-29 12:45:21 +0100

    I learned how to use in game random sprites

--- Game/src/main/GameScreen.java
+++ Game/src/main/GameScreen.java
@@ -3,27 +3,43 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel {
     private Random random;
     private BufferedImage img;
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
     public GameScreen(BufferedImage img){
         random = new Random();
         this.img = img;
+        loadSprites();
 
+    }
+
+    private void loadSprites() {
+        for (int y = 0; y < 10; y++){
+            for (int x = 0; x < 10; x++){
+                sprites.add(img.getSubimage(x * 32,y * 32,32,32));
+            }
+        }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(img, 0, 0, null);
+//        g.drawImage(sprites.get(19), 0, 0, null);
+//        g.drawImage(img.getSubimage(32*9, 32, 32, 32), 0, 0, null);
 
-//        for (int x=0; x < 20; x++){
-//            for(int y=0; y<20; y++){
-//            g.setColor(getRandomColor());
-//            g.fillRect(x*32, y*32, 32,32);
-//        }}
+        for (int x=0; x < 20; x++){
+            for(int y=0; y<20; y++){
+            g.setColor(getRandomColor());
+        g.drawImage(sprites.get(getRandomInt()), x*32, y*32, null);
+
+        }}
+    }
+    private int getRandomInt(){
+        return random.nextInt(20);
     }
     private Color getRandomColor(){
         int r = random.nextInt(256);
